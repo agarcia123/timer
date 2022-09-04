1f049fe23333fb996c25375a6c7dc087b496abf5
augusto <gariaaugusto@gmail.com>
2022-09-04 19:01:51 -0400

    feature: created  2 css cotainers

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import './App.css';
 import React, {Component} from 'react';
 
-
-
-
-
-
 class App extends Component {
 
-  //const d = new Date();
-  //let time = d.getTime();
-
   state = {
-    minutes: 1,
+    minutes: 0,
     seconds: 0
   }
 
   timer() {
-          this.pija = setInterval(() => {
+          this.time = setInterval(() => {
               const { seconds, minutes } = this.state
 
               if (seconds > 0) {
-                  this.setState( ({ seconds }) => ({
-                      seconds: seconds - 1
-                  })
-                )
+                  this.setState( ({ seconds }) => ({seconds: seconds - 1}))
               }
               if (seconds === 0) {
                   if (minutes === 0) {
-                      clearInterval(this.pija)
+                      clearInterval(this.time)
                   } else {
                       this.setState(({ minutes }) => ({
                           minutes: minutes - 1,
@@ -71,24 +60,19 @@ class App extends Component {
 
 
   render () {
-     const { minutes, seconds } = this.state
-
-
-
+    const { minutes, seconds } = this.state
     return (
-      <div className="App">
-
-          <div>
-
-            <button onClick={() => this.reset() } >reset</button>
-            <button onClick={() => this.timer()} >start</button>
-            <button onClick={() => this.add()} >+</button>
-            <button onClick={() => this.substract()} >-</button>
-            <button onClick={() =>clearInterval(this.pija) }>stop</button>
-          </div>
-
-          <div>
-          <h1>Time Remaining: { minutes }:{ seconds < 10 ? `0${ seconds }` : seconds }</h1>
+      <div className="float-container">
+          <div className="float-child-buttons">
+             <button onClick={() => this.reset() } >reset</button>
+             <button onClick={() => this.timer()} >start</button>
+             <button onClick={() => this.add()} >+</button>
+             <button onClick={() => this.substract()} >-</button>
+             <button onClick={() =>clearInterval(this.time) }>stop</button>
+        </div>
+        <div className="float-child-time">
+          <h1>Time Remaining:</h1>
+          <h1 className="time">{ minutes }:{ seconds < 10 ? `0${ seconds }` : seconds }</h1>
         </div>
     </div>
   );
